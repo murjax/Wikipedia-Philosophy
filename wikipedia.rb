40fe64cb6a0d8efb1f2d2519b7f396b914a80b36
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-11-25 13:25:59 -0500

    add limit of 5

--- wikipedia.rb
+++ wikipedia.rb
@@ -3,9 +3,9 @@ require 'HTTParty'
 require 'mechanize'
 
 def search_link(page, visited_pages)
-  index = 0
   link = nil
 
+  index = 0
   while !link && !visited_pages.include?(page.title)
     link = page.search('//div[@class="mw-parser-output"]/p/a')[index].attributes["href"].value
     index += 1
@@ -20,11 +20,13 @@ mechanize = Mechanize.new
 page = mechanize.get("http://wikipedia.org/wiki/#{page_title}")
 visited_pages = []
 
-while true do
+index = 0
+while true && index < 5 do
   puts page.title
   first_link = search_link(page, visited_pages)
   next_page = "http://www.wikipedia.org/#{first_link}"
   visited_pages.push(page.title)
   page = mechanize.get(next_page)
+    index += 1
 end
 
