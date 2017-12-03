1975d0f21f0106f2767a699fab7309f8b247f9d3
Ryan Murphy <murjax@gmail.com>
2017-12-03 14:36:46 -0500

    Convert wikipedia to a class

--- wikipedia.rb
+++ wikipedia.rb
@@ -3,19 +3,25 @@ require 'HTTParty'
 require_relative 'input'
 require_relative 'page'
 
-def start
-  visited_pages = [];
-  page_title = Input.get_page_title;current_page = Page.new(page_title, visited_pages);
-
-  while current_page.title != "Philosophy - Wikipedia" do
-    puts current_page.title
-    next_page_title = current_page.search_link
-    visited_pages.push(current_page.uri)
-    current_page = Page.new(next_page_title, visited_pages)
+class Wikipedia
+  def initialize
+    @visited_pages = []
+    @page_title = Input.get_page_title
+    @current_page = Page.new(@page_title, @visited_pages);
+    start
   end
 
-  puts current_page.title
-  puts "Done!"
+  def start
+    while @current_page.title != "Philosophy - Wikipedia" do
+      puts @current_page.title
+      next_page_title = @current_page.search_link
+      @visited_pages.push(@current_page.uri)
+      @current_page = Page.new(next_page_title, @visited_pages)
+    end
+
+    puts @current_page.title
+    puts "Done!"
+  end
 end
 
-start
+wikipedia_search = Wikipedia.new
